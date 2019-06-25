f6e8a53dbb64d450f1900f3a62945101374f2341
khangnguyen <khang.nguyen@giosg.com>
2019-06-25 14:56:31 +0300

    Create setup test file standalone

---
+++ runtests.py
@@ -0,0 +1,35 @@
+import os
+import sys
+APP_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, APP_DIR)
+SETTINGS_DICT = {
+    'INSTALLED_APPS': ('giosgapps_bindings',),
+    'ROOT_URLCONF': 'giosgapps_bindings.tests.urls',
+    'DATABASES': {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        },
+    }
+}
+
+def run_tests():
+
+    from django.conf import settings
+    settings.configure(**SETTINGS_DICT)
+
+
+    import django
+    django.setup()
+
+
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+
+    test_runner = TestRunner(verbosity=2, interactive=True)
+    failures = test_runner.run_tests(['giosgapps_bindings.tests'])
+    sys.exit(failures)
+
+
+if __name__ == '__main__':
+    run_tests()
