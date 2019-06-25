b0c46dc1d1a176089a844b32b96142de7d01b349
Mew-www <mew.proxy@hotmail.com>
2019-06-25 14:10:05 +0300

    Implement ApplicationTriggerView baseview

---
+++ giosgapps_bindings/views/application_trigger_view.py
@@ -0,0 +1,54 @@
+from django.views import View
+from django.http import HttpResponseBadRequest
+from django.conf import settings
+from ..lib.giosg_trigger_in_django import GiosgTriggerInDjango
+
+
+class ApplicationTriggerView(View):
+    http_method_names = ['get']
+
+    def get(self, request):
+        # Leave validation to GiosgTriggerInDjango object
+        try:
+            trigger = GiosgTriggerInDjango(request, settings.GIOSG_APP_SECRET)
+            handler = getattr(self, 'on_'+trigger.type, self.__unsupported_trigger_type)
+            return handler(request, trigger)
+        # Handle any trigger-data validation errors
+        except ValueError as e:
+            return HttpResponseBadRequest(e)
+
+    def __unsupported_trigger_type(self, *args, **kwargs):
+        return HttpResponseBadRequest('Unsupported trigger type')
+
+    def on_install(self, request, trigger):
+        raise NotImplementedError
+
+    def on_setup(self, request, trigger):
+        raise NotImplementedError
+
+    def on_uninstall(self, request, trigger):
+        raise NotImplementedError
+
+    def on_chat_start(self, request, trigger):
+        raise NotImplementedError
+
+    def on_chat_end(self, request, trigger):
+        raise NotImplementedError
+
+    def on_console_load(self, request, trigger):
+        raise NotImplementedError
+
+    def on_manual_dialog(self, request, trigger):
+        raise NotImplementedError
+
+    def on_manual_nav(self, request, trigger):
+        raise NotImplementedError
+
+    def on_chat_open(self, request, trigger):
+        raise NotImplementedError
+
+    def on_chat_close(self, request, trigger):
+        raise NotImplementedError
+
+    def on_chat_focus(self, request, trigger):
+        raise NotImplementedError
