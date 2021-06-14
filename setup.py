a6d64f706fba62d0928146cca194f9d0aeb1d2d4
JuusoLas <juuso.lassila@giosg.com>
2021-06-14 12:28:45 +0300

    increased version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     setup(
       name='giosgapps_bindings',
       packages=setuptools.find_packages(),
-      version='0.1.0',
+      version='0.1.1',
       license='MIT',
       description='Module for Giosg Apps development',
       author='Giosg',
